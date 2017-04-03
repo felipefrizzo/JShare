c43de5e8d48bbe80421f17e838586df1298ef4bb
Felipe Frizzo <felipefrizzo@gmail.com>
2017-04-03 19:21:49 -0300

    Update MD5 file validation

--- src/main/java/br/univel/jshare/validator/MD5Validator.java
+++ src/main/java/br/univel/jshare/validator/MD5Validator.java
@@ -1,32 +1,55 @@
 package br.univel.jshare.validator;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Objects;
 
 /**
  * Created by felipefrizzo on 03/04/17.
  */
-public class MD5Validator implements GenericValidator {
-    @Override
-    public String Validation(String validate) {
-        Objects.requireNonNull(validate, "Validate cannot be null");
-
-        MessageDigest messageDigest;
-        StringBuilder string = new StringBuilder();
-
+public class MD5Validator {
+    public static byte[] createChecksum(String filename) {
         try {
-            messageDigest = MessageDigest.getInstance("MD5");
-            messageDigest.update(validate.getBytes());
+            InputStream input = new FileInputStream(filename);
 
-            byte[] digest = messageDigest.digest();
-            for (byte b: digest) {
-                string.append(String.format("%02x", b & 0xff));
-            }
+            byte[] buffer = new byte[1024];
+            MessageDigest complete = MessageDigest.getInstance("MD5");
+            int numRead;
 
-            return string.toString();
+            do {
+                numRead = input.read(buffer);
+                if (numRead > 0) {
+                    complete.update(buffer, 0, numRead);
+                }
+            } while (numRead != -1);
+
+            input.close();
+            return complete.digest();
+
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public static String getMD5Checksum(String filename) {
+        byte[] bytes;
+        try {
+            bytes = createChecksum(filename);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String result = "";
+
+        for (int i = 0; i < bytes.length; i++) {
+            result += Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return result;
     }
 }
